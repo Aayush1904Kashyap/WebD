470d4f512996813682d4a0775fe98ae4f6c27d39
AAYUSH <aayushkashyap617@gmail.com>
2025-07-26 15:54:44 +0530

    operations added

--- JS/01.basics/3.conversionOps.js
+++ JS/01.basics/3.conversionOps.js
@@ -5,8 +5,8 @@ console.log(typeof score);
 console.log(typeof (score)); 
 
 let valueInNumber = Number(score); // convert to number
-console.log(typeof valueInNumber); // number 
-console.log(valueInNumber); // NaN (Not a Number)
+// console.log(typeof valueInNumber); // number 
+// console.log(valueInNumber); // NaN (Not a Number)
 
 //"33" => 33
 //"33abc" => NaN
@@ -16,9 +16,28 @@ console.log(valueInNumber); // NaN (Not a Number)
 // to boolean
  let isloggedIn = "aayush"
  let isLoggedInBoolean = Boolean(isloggedIn);
- console.log(isLoggedInBoolean); 
+//  console.log(isLoggedInBoolean); 
 
  // 1=> true
  // 0=> false  
  //"" => false
  //aayush=> true
+
+
+
+
+
+ //***************OPERATIONS***************/
+
+ let val=69;
+ let negval=-val; // -69
+
+let str1="aayush";
+let str=" kashyap";
+let fullName = str1 + str; // "aayush kashyap"
+
+console.log("1"+2); // "12" (string concatenation)
+console.log(1+"2"); // "12" (string concatenation)
+console.log("1"+2+3); // "123" (string concatenation)
+console.log(1+2+"3"); // "33" (number addition then string concatenation)
+console.log(1+2+3); // 6 (number addition)
